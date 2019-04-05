2bd17dbddbb56d8555ac6897e20859acc5cc1cdb
artkwi <artkwi@st.amu.edu.pl>
2019-04-05 13:25:31 +0200

    Basic template added.

--- library/src/App.js
+++ library/src/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
+import Books from './Components/Books';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="container">
         <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+            <h1>Library</h1>
         </header>
+        <main>
+          <div className="book-details">
+          <h2>Book details</h2>
+          </div>
+          <div className="books-list"> 
+          <h2>Books:</h2>
+          <Books />
+          </div>
+        </main>
       </div>
     );
   }
---
+++ library/src/Components/Books.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Books = props  => (
+    <div className="books-list-wrapper">
+    <header className="books-list-header">
+        <div>Name:</div>
+        <div>Author:</div>
+        <div>Description:</div>
+    </header>
+    <section>
+        <div></div>
+        <div></div>
+        <div></div>
+    </section>
+    </div>
+);
+
+export default Books;
