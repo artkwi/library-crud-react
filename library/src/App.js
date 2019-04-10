59526d7405f3e188f8f7d30ed354dc7a5bcdbae7
artkwi <artkwi@st.amu.edu.pl>
2019-04-10 13:54:00 +0200

    Delete request modified

--- library/src/App.js
+++ library/src/App.js
@@ -1,9 +1,8 @@
-import React, { Component, useState  } from 'react';
-import Books from './Components/Books';
-import Basic from './Components/AddBook';
+import React, { Component, useState } from "react";
+import Books from "./Components/Books";
+import Basic from "./Components/AddBook";
 
 class App extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
@@ -14,61 +13,69 @@ class App extends Component {
     };
   }
 
-
-
-
   componentDidMount() {
     fetch("http://localhost:3004/books")
       .then(res => res.json())
       .then(
-        (result) => {
+        result => {
           this.setState({
             isLoaded: true,
             books: result
           });
         },
-        (error) => {
+        error => {
           this.setState({
             isLoaded: true,
             error: error
           });
         }
-      )
+      );
   }
 
-
   render() {
-    return ( 
+    return (
       <div className="container">
         <header className="App-header">
-            <h1>Library</h1>
+          <h1>Library</h1>
         </header>
         <main>
           <div className="book-details">
-          <h2>Book details</h2>
-          <Basic />
+            <h2>Book details</h2>
+            <Basic />
           </div>
-          <div className="books"> 
-          <h2>Books:</h2>
-          <Books books={this.state.books} removeBook={this.removeBook} editBook={this.editBook}/>
+          <div className="books">
+            <h2>Books:</h2>
+            <Books
+              books={this.state.books}
+              removeBook={this.removeBook}
+              editBook={this.editBook}
+            />
           </div>
         </main>
       </div>
     );
   }
 
-removeBook = (book) => {
-    console.log(book);
+  removeBook = book => {
+    // API delete request
+    fetch("http://localhost:3004/books/" + this.state.books[book].id, {
+      method: "delete",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      }
+    });
+    console.log(this.state.books[book]);
     const books = [...this.state.books];
-    books.splice(book,1);
-    this.setState({books: books});
-  }
+    books.splice(book, 1);
+    this.setState({ books: books });
+  };
 
-editBook = (book) => {
-  console.log(book);
-}
+  editBook = book => {
+    console.log(book);
+  };
 
-
+  updateBooks;
 }
 
 export default App;
